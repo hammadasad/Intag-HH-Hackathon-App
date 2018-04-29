ccd5b6cd9f6a3c3145208a97c4e6d43a9c4af737
Hammad Asad <hammad_asad@msn.com>
2018-04-29 02:09:15 -0400

    Added Controllers & Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'prescriptions#index'
+
+  get 'sessions/new'
+  post 'sessions/create'
+  delete 'sessions/destroy'
+
+  get 'users/new'
+  post 'users/create'
 end
